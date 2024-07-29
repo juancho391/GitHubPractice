4e41b604411c406bec82bf16a714c878947778ea
Juan Bedoya <juan@Juans-MacBook-Air.local>
2024-07-28 22:13:34 -0500

    se modifica entidad users, se hace un llamado a la api de google books usando el modulo requests de python

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("Hello world")
+from models import book, user
+import requests
+
+url = "https://www.googleapis.com/books/v1/volumes"
+
+
+# URL del endpoint de búsqueda de la API de Google Books
+url = "https://www.googleapis.com/books/v1/volumes"
+
+# Parámetros de búsqueda
+params = {
+    'q': 'Python',  # Consulta de búsqueda
+    'maxResults': 5  # Número máximo de resultados a devolver
+}
+
+# Realizar la solicitud GET
+response = requests.get(url, params=params)
+
+# Verificar el estado de la respuesta
+if response.status_code == 200:
+    # Convertir la respuesta en JSON
+    data = response.json()
+    
+    # Imprimir los títulos de los libros encontrados
+    for item in data.get('items', []):
+        title = item['volumeInfo'].get('title', 'No title')
+        print(f"Title: {title}")
+else:
+    print(f'Error: {response.status_code}')
+
+
---
+++ models/user.py
@@ -0,0 +1,7 @@
+class User:
+
+    def __init__(self,name,password,UserID,mail) -> None:
+        self.name = name
+        self.password = password
+        self.uid = UserID
+        self.mail = mail
