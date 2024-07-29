215fc9746d3b9499b92346344474456870567295
Juan Bedoya <juan@Juans-MacBook-Air.local>
2024-07-28 20:59:07 -0500

    add class Book in models.py

---
+++ models.py
@@ -0,0 +1,13 @@
+#Book class
+
+
+class Book:
+
+    def __init__(self,name,author,average) -> None:
+        self.name = name
+        self.author = author
+        self.average = average
+
+
+        
+
