906fcf49322f5f15eac67998132198e5f2b56bfe
Juan Bedoya <juan@Juans-MacBook-Air.local>
2024-07-28 21:47:04 -0500

    add folder models,change name models to book, modify atributs of class book

---
+++ models/book.py
@@ -0,0 +1,17 @@
+#Book class
+
+
+class Book:
+
+    def __init__(self,pId,title,subtitle,author,published,average) -> None:
+        self.id = pId
+        self.title = title
+        self.subtitle = subtitle
+        self.author = author
+        self.published = published
+        self.average = average
+
+
+
+
+
